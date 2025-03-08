513874124d809dacb3781cd32d8deac60ce077f1
Vuietkie <vuhiep27082005@gmail.com>
2025-03-08 15:59:01 +0700

    Update detail

--- pages/Detail.vue
+++ pages/Detail.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="min-h-screen" :style="{ backgroundImage: `url('/images/background.jpg')`, backgroundRepeat: 'repeat' }">
     <Header />
-    <FilterBar />
     <DetailProduct />
     <Footer />
   </div>
